6ce0d27afc53f9d7108015ef7d9e4efd72a0af7a
Jacob Powers <jpowers@23andme.com>
2018-05-30 15:15:09 -0700

    add basic usage of sound generation

---
+++ sound.py
@@ -0,0 +1,63 @@
+import wave
+import array
+import random
+
+import requests
+from urllib.parse import urlencode
+
+
+DURATION = 3  # seconds
+FRAMERATE = 44100
+SAMPLE_WIDTH = 2  # 2 bytes, 16bit audio
+
+
+def fetch_remote_noise():
+    query = urlencode({
+        'num': 10**4,
+        'min': - 2 ** (SAMPLE_WIDTH / 2),
+        'max': 2 ** (SAMPLE_WIDTH * 8 / 2),
+        'base': '10',
+        'format': 'plain',
+        'col': 1,
+    })
+    url = f'https://www.random.org/integers/?{query}'
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise Exception(f"Invalid response, got status: {res.status_code}")
+    return [int(v) for v in res.content.split(b'\n') if v]
+
+
+def native_noise_generator(samples):
+    i = 0
+    while i < samples:
+        yield random.randint(-2 ** 8, 2 ** 8)
+        i += 1
+
+
+def remote_noise_generator(samples):
+    i = 0
+    pool_idx = 1
+    random_pool = []
+    while i < samples:
+        if pool_idx >= len(random_pool):
+            pool_idx = 0
+            random_pool = fetch_remote_noise()
+        yield random_pool[pool_idx]
+        i += 1
+        pool_idx += 1
+
+
+def write_wav(filename, audio_generator):
+    with wave.open(filename, 'w') as w:
+        w.setnchannels(1)
+        w.setframerate(FRAMERATE)
+        w.setsampwidth(2)
+        data = array.array('h')
+        for value in audio_generator(DURATION * FRAMERATE):
+            data.append(int(value))
+        w.writeframes(data)
+
+
+if __name__ == '__main__':
+    write_wav('./native_noise.wav', native_noise_generator)
+    write_wav('./remote_noise.wav', remote_noise_generator)
---
+++ test_sound.py
@@ -0,0 +1,4 @@
+import sound
+
+
+def test_randomly_generated_sound():
